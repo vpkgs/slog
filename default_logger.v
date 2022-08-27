9515b8d4aa0d806dce0f8197c6b80a55477eaf83
Ken <ken0x0a+github@gmail.com>
2022-08-27 14:06:20 +0900

    fix: typo

--- default_logger.v
+++ default_logger.v
@@ -3,7 +3,7 @@ module slog
 import os
 import time
 
-pub struct DefaultLoggger {
+pub struct DefaultLogger {
 	BaseLogger
 	ModLevelMapString
 	ofname_prefix string
@@ -13,15 +13,15 @@ mut:
 }
 
 [params]
-pub struct DefaultLogggerOpt {
+pub struct DefaultLoggerOpt {
 pub:
 	level Level = Level.info
 }
 
-pub fn init_with_default_logger(ofname string, opt DefaultLogggerOpt) &DefaultLoggger {
+pub fn init_with_default_logger(ofname string, opt DefaultLoggerOpt) &DefaultLogger {
 	name := '$ofname-0.log'
 	ofile := os.open_append(name) or { panic('couldn\'t opening log file $name for appending') }
-	mut log := &DefaultLoggger{
+	mut log := &DefaultLogger{
 		ofname_prefix: ofname
 		ofname: name
 		ofile: ofile
@@ -31,14 +31,14 @@ pub fn init_with_default_logger(ofname string, opt DefaultLogggerOpt) &DefaultLo
 	return log
 }
 
-pub fn (lg &DefaultLoggger) log(lv Level, target string, msg string) {
+pub fn (lg &DefaultLogger) log(lv Level, target string, msg string) {
 	if lg.enabled(target, lv) { // TODO: move inside `slog.log()` (segfault...)
 		lg.log_file(lv, target, msg)
 		lg.log_console(lv, target, msg)
 	}
 }
 
-fn (lg &DefaultLoggger) log_file(lv Level, target string, msg string) {
+fn (lg &DefaultLogger) log_file(lv Level, target string, msg string) {
 	mut l := unsafe { lg }
 
 	timestamp := time.now().format_ss_micro()
@@ -50,7 +50,7 @@ fn (lg &DefaultLoggger) log_file(lv Level, target string, msg string) {
 	l.ofile.flush()
 }
 
-fn (lg &DefaultLoggger) log_console(lv Level, target string, msg string) {
+fn (lg &DefaultLogger) log_console(lv Level, target string, msg string) {
 	timestamp := time.now().format_ss_micro()
 	level := lg.fmt_level_for_term(lv)
 	println('$timestamp $level $target: $msg')
